68c3a80764d89211b4606cdfddb417050c6df0b8
ZhichGaming <nick.zhicheng@gmail.com>
2022-08-26 01:49:35 -0400

    Initial Commit

---
+++ Multitainment/MultitainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultitainmentApp.swift
+//  Multitainment
+//
+//  Created by Nick on 2022-08-26.
+//
+
+import SwiftUI
+
+@main
+struct MultitainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
