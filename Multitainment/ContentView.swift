d5ea8b1807177fae9b3d4ea0fdccbd6bc09a8dc4
ZhichGaming <nick.zhicheng@gmail.com>
2022-08-29 01:25:58 -0400

    Reworked UI, fixed logic

--- Multitainment/ContentView.swift
+++ Multitainment/ContentView.swift
@@ -15,6 +15,8 @@ struct ContentView: View {
     @State private var currentQuestionLocation = 0
     @State private var answer: Int?
     @State private var gameStarted = false
+    @State private var isVerified = false
+    @State var isCorrect = false
     
     @State private var showingAlert = false
     @State private var alertTitle = ""
@@ -25,38 +27,35 @@ struct ContentView: View {
     
     var body: some View {
         NavigationView {
-            List {
+            VStack(alignment: .center) {
+                Text("Multitainment")
+                    .font(.largeTitle.weight(.heavy))
                 SettingsView(minimumMultiplication: $minimumMultiplication, maximumMultiplication: $maximumMultiplication, questionCountSelection: $questionCountSelection, questionCount: $questionCount)
+                Divider()
+                    .padding()
+                Spacer()
                 
-                QuestionsView(questionCountSelection: $questionCountSelection, maximumMultiplication: $maximumMultiplication, minimumMultiplication: $minimumMultiplication, showingAlert: $showingAlert, alertTitle: $alertTitle, alertMessage: $alertMessage, questions: $questions, currentQuestion: $currentQuestion, currentQuestionLocation: $currentQuestionLocation, answer: $answer, gameStarted: $gameStarted)
+                QuestionsView(questionCountSelection: $questionCountSelection, maximumMultiplication: $maximumMultiplication, minimumMultiplication: $minimumMultiplication, showingAlert: $showingAlert, alertTitle: $alertTitle, alertMessage: $alertMessage, questions: $questions, currentQuestion: $currentQuestion, currentQuestionLocation: $currentQuestionLocation, answer: $answer, gameStarted: $gameStarted, isVerified: $isVerified, isCorrect: $isCorrect)
+                Spacer()
             }
-            .navigationTitle("Multitainment")
             .listStyle(.grouped)
             .toolbar {
-                Button("Start", action: startGame)
             }
             .alert(alertTitle, isPresented: $showingAlert) {
                 Button("OK") {}
-
             } message: {
                 Text(alertMessage)
             }
+            .background(LinearGradient(
+                colors: [.purple, .mint],
+                startPoint: .top,
+                endPoint: .bottom)
+                .edgesIgnoringSafeArea(.all))
         }
+        .foregroundColor(.white)
     }
     
-    func startGame() {
-        questions.removeAll()
-        gameStarted = true
-        
-        // Repeats for number of questions
-        for _ in 0..<(Int(questionCountSelection) ?? 5) {
-            // question is the question generator
-            let question = "\(Int.random(in: minimumMultiplication...maximumMultiplication)) * \(Int.random(in: 1...12))"
-            questions.append(question)
-            
-            currentQuestion = questions[currentQuestionLocation]
-        }
-    }
+
 }
 
 struct SettingsView: View {
@@ -67,29 +66,55 @@ struct SettingsView: View {
     @Binding var questionCount: [String]
     
     var body: some View {
-        // Minimum and maximum multiplication tables
-        Section {
-            Stepper("From \(minimumMultiplication)", value: $minimumMultiplication, in: 1...20)
-                .onChange(of: minimumMultiplication) { _ in
-                    if minimumMultiplication >= maximumMultiplication { minimumMultiplication -= 1 }
+        Group {
+            // Minimum and maximum multiplication tables
+            Section {
+                ZStack {
+                    RoundedRectangle(cornerRadius: 15)
+                        .shadow(radius: 5)
+
+                    VStack {
+                        Stepper("From \(minimumMultiplication)", value: $minimumMultiplication, in: 1...20)
+                            .onChange(of: minimumMultiplication) { _ in
+                                if minimumMultiplication >= maximumMultiplication { minimumMultiplication -= 1 }
+                            }
+                        Stepper("To \(maximumMultiplication)", value: $maximumMultiplication, in: 1...20)
+                            .onChange(of: maximumMultiplication) { _ in
+                                if minimumMultiplication >= maximumMultiplication { maximumMultiplication += 1 }
+                            }
+                    }
+                    .foregroundColor(.black)
+                    .frame(maxWidth: .infinity)
+                    .padding(.leading, 10)
+                    .padding(.all, 10)
                 }
-            Stepper("To \(maximumMultiplication)", value: $maximumMultiplication, in: 1...20)
-                .onChange(of: maximumMultiplication) { _ in
-                    if minimumMultiplication >= maximumMultiplication { maximumMultiplication += 1 }
-                }
-        } header: {
-            Text("Multiplication tables")
-        }
-        // Number of questions per round
-        Section {
-            Picker("Number of questions", selection: $questionCountSelection) {
-                ForEach(questionCount, id: \.self) {
-                    Text($0)
-                }
+                .fixedSize(horizontal: false, vertical: true)
+            } header: {
+                Text("Multiplication tables")
+                    .font(.headline)
             }
-            .pickerStyle(.segmented)
-        } header: {
-            Text("Number of questions")
+            .padding(.horizontal)
+
+            // Number of questions per round
+                Section {
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 15)
+                            .shadow(radius: 5)
+                        
+                        Picker("Number of questions", selection: $questionCountSelection) {
+                            ForEach(questionCount, id: \.self) {
+                                Text($0)
+                            }
+                        }
+                        .pickerStyle(.segmented)
+                        .padding(10)
+                    }
+                    .fixedSize(horizontal: false, vertical: true)
+                    .padding(.horizontal)
+                } header: {
+                    Text("Number of questions")
+                        .font(.headline)
+                }
         }
     }
 }
@@ -111,28 +136,74 @@ struct QuestionsView: View {
     @Binding var currentQuestionLocation: Int
     @Binding var answer: Int?
     @Binding var gameStarted: Bool
+    @Binding var isVerified: Bool
+    @Binding var isCorrect: Bool
 
 
     var body: some View {
-        Section {
-            Text(currentQuestion)
-                .font(.headline)
+        VStack {
+            Spacer()
+            Text(currentQuestion == "" ? "Press Start to start" : currentQuestion)
+                .font(.title.bold())
                 .onChange(of: currentQuestion) { _ in
                     print("text: \(currentQuestion)")
                 }
-            TextField("Enter your answer", value: $answer, format: .number)
-                .keyboardType(.numberPad)
-                .focused($isInputActive)
-                .toolbar {
-                    ToolbarItemGroup(placement: .keyboard) {
-                        Spacer()
+            ZStack {
+                RoundedRectangle(cornerRadius: 15)
+                    .foregroundColor(isVerified ? (isCorrect ? .green : .red) : .white)
+                    .animation(.default, value: isVerified)
+                    .animation(.default, value: isCorrect)
+                    .shadow(radius: 5)
 
-                        Button("Done") {
-                            isInputActive = false
-                            continueGame()
+                
+                TextField("Enter your answer", value: $answer, format: .number)
+                    .keyboardType(.numberPad)
+                    .focused($isInputActive)
+                    .toolbar {
+                        ToolbarItemGroup(placement: .keyboard) {
+                            Spacer()
+
+                            Button("Done") {
+                                isInputActive = false
+                            }
+                            .foregroundColor(.none)
                         }
                     }
-                }
+                    .multilineTextAlignment(.center)
+                    .padding()
+                    .padding(.horizontal)
+                    .foregroundColor(.black)
+            }
+            .fixedSize(horizontal: true, vertical: true)
+            
+            Spacer()
+            // Button will display and execute Continue if the game started and is verified, Verify if not and start otherwise.
+            Button(gameStarted ? (isVerified && isCorrect ? "Continue" : "Verify") : "Start", action: gameStarted ? continueGame : startGame)
+                .frame(minWidth: 0, maxWidth: .infinity)
+                .font(.system(size: 18))
+                .padding()
+                .overlay(
+                    RoundedRectangle(cornerRadius: 15)
+                        .stroke(Color.white, lineWidth: 2)
+                )
+                .padding()
+                .shadow(radius: 5)
+
+        }
+        .padding(.horizontal)
+    }
+    
+    func startGame() {
+        questions.removeAll()
+        gameStarted = true
+        
+        // Repeats for number of questions
+        for _ in 0..<(Int(questionCountSelection) ?? 5) {
+            // question is the question generator
+            let question = "\(Int.random(in: minimumMultiplication...maximumMultiplication)) * \(Int.random(in: 1...12))"
+            questions.append(question)
+            
+            currentQuestion = questions[currentQuestionLocation]
         }
     }
     
@@ -142,15 +213,13 @@ struct QuestionsView: View {
         showingAlert = true
     }
     
-    func isCorrect() -> Bool {
+    func verifyAnswer() -> Bool {
         let expression = NSExpression(format: currentQuestion)
         let value = expression.expressionValue(with: nil, context: nil) as? Int
         
         // Display an alert if incorrect
         if (answer ?? 0) != value {
-            alertTitle = "Incorrect"
-            alertMessage = "Try again!"
-            showingAlert = true
+
             return false
         }
         return true
@@ -166,8 +235,12 @@ struct QuestionsView: View {
             return
         }
         
-        // Checks if answer is correct
-        if isCorrect() {
+        // Checks if answer has been verified
+        if isVerified {
+            if !isCorrect {
+                isVerified = false
+                return
+            }
             currentQuestionLocation += 1
 
             // Checks if this is the last question
@@ -177,13 +250,21 @@ struct QuestionsView: View {
                 return
             }
             currentQuestion = questions[currentQuestionLocation]
-        } else {
-            // If the answer is wrong, add a mistake and exit.
-            mistakes += 1
+            answer = nil
+            isVerified = false
+            isCorrect = false
             return
-        
         }
-        answer = nil
+        isCorrect = verifyAnswer()
+        
+        // Checks if answer is correct
+        if isCorrect {
+            isVerified = true
+        } else {
+            // If the answer is wrong, add a mistake.
+            mistakes += 1
+            isVerified = true
+        }
     }
     
     func resetGame() {
@@ -193,6 +274,8 @@ struct QuestionsView: View {
         currentQuestion = ""
         currentQuestionLocation = 0
         gameStarted = false
+        isVerified = false
+        isCorrect = false
     }
 }
 
